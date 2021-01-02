e78c777a79ba14442269c2e3abcafc301f282c88
Muhammet Derviş Aygan <68205028+dervisaygan@users.noreply.github.com>
2021-01-02 13:52:30 +0300

    Add files via upload

---
+++ index/script.js
@@ -0,0 +1,526 @@
+var soru1= [
+    1, // soru numarası
+
+    `5 litrelik bir sürahide bulunan suyun tamamı
+    Birbirine eş 8 bardağa eşit olarak dolduruluyor.
+    Buna göre bardakların birindeki su miktarı
+    Aşağıdakilerden hangisinde doğru olarak
+    Verilmiştir?
+    `, // soru
+    ['0,8 litre','0.62 litre','0,625 litre','0,5 litre'], // şıklar
+    '0,625'
+]
+var soru2= [
+    2, // soru numarası
+
+    `2,35 ondalık gösteriminin çözümlenmiş hali aşağıdakilerden hangisidir?`, // soru
+    ['a 2 x 1 + 3 x 0,1 + 5 x 0,01','b 2 x 0,1 + 3 x 0,01 + 5 x 0,001','c 2 x 10 + 3 x 1 + 5 x 0,1','d 2 x 10 + 3 x 0,1 + 5 x 0,01'], // şıklar
+    'a'
+]
+var soru3= [
+    3, // soru numarası
+
+    '42,483 ondalık gösteriminin onda birler basamağına göre yuvarlanmış hâli aşağıdakilerden hangisidir?', // soru
+    ['42,4','42,5','42,45','42,480'], // şıklar
+    '42,5'
+]
+var soru4= [
+    4, // soru numarası
+
+    `Kare pist => 0,8 km <br>
+    Oval pist => 0,22 km<br>
+    Üçgen pist => 0,96 km<br>
+    Düz pist => 0,75 km<br>
+    Sezin sağlıklı yaşam için her sabah mutlaka koşmaktadır. Koşmak için farklı pistler tercih eden Sezin ilk gün kare pistte 2,5 tur, ikinci gün oval pistte 5 tur, üçüncü gün üçgen pistte 2 tur, son günde düz pistte 5 tur atmıştır. 
+    Buna göre Sezin 4 gün sonunda toplamda kaç kilometre koşmuştur?
+    `, // soru
+    ['8,53','8,77','9,13','9,62'], // şıklar
+    '8,77'
+]
+
+
+
+
+
+
+
+
+var soru5= [
+    1, // soru numarası
+
+    `Bir kenarı 27,8 cm olan eşkenar üçgenin çevre uzunluğu kaç cm dir?
+    `, // soru
+    ['81,24','82,4','83,4','83,24'], // şıklar
+    '83,4'
+]
+var soru6= [
+    2, // soru numarası
+
+    `“Beş tam binde yirmi altı”
+    Yukarıda okunuşu verilen ondalık gösterimin çözümlenmiş hali aşağıdakilerden hangisidir?`, // soru
+    ['a 5 x 10 + 2 x 0,1 + 6 x 0,01','b 5 x 1 + 2 x 0,1 + 6 x 0,01','c 5 x 10 + 2 x 0,01 + 6 x 0,001','d 5 x 1 + 2 x 0,01 + 6 x 0,001'], // şıklar
+    'd'
+]
+var soru7= [
+    3, // soru numarası
+
+    '2, 3, 4, 7 ve 8 rakamlarıyla oluşturulan ondalık gösterimin onda birler basamağına yuvarlanışı 34,8 ise bu sayı hangisi olabilir?', // soru
+    ['34,87','34,47','34,78','34,72'], // şıklar
+    '34,78'
+]
+var soru8= [
+    4, // soru numarası
+
+    'Melik klavyesini temizlemeye çalışırken tüm tuşları yanlışlıkla yere düşürmüştür. Tuşları geri yerine koymak isteyen melik her bir satırdaki tuşun uzunluğunun farklı olduğunu gözlemlemiştir. İlk satırda tanesi 0,8 cm olan 20 tuş, ikinci satırda tanesi 1,2 cm olan 22 tuş ve üçüncü satırda tanesi 1,43 cm olan 17 tuş vardır. Melik tuşları geri takabilmek için arasında en fazla boy farkı olan satırı bulmalıdır. Buna göre aralarında en fazla uzunluk farkı olan satırların arasında kaç cm vardır?', // soru
+    ['12,24','13,4','10,4','10,24'], // şıklar
+    '10,4'
+]
+
+
+
+
+
+
+var soru9= [
+    1, // soru numarası
+
+    'Dört kardeş bayramda topladıkları 84,80 lirayı eşit olarak paylaşmışlardır. Buna göre bir kişi kaç lira almıştır?', // soru
+    ['42,4','21,2','21,4','21,44'], // şıklar
+    '21,2'
+]
+var soru10= [
+    2, // soru numarası
+
+    'Aşağıdaki ondalık gösterimlerin hangisinde 3’ün basamak değeri en büyüktür?', // soru
+    ['43,025','75,023','82,349','9,637'], // şıklar
+    '43,025'
+]
+var soru11= [
+    3, // soru numarası
+
+    `l. 41,385 II. 41,382 III. 41,393 IV. 41,397 
+    Yukarıda verilen ondalık gösterimlerin hangilerinin yüzde birler basamağına göre yuvarlanmış hâli eşittir?
+    `, // soru
+    ['a I ve II','b I ve III','c II ve IV','d III ve IV'], // şıklar
+    'b'
+]
+var soru12= [
+    4, // soru numarası
+
+    `Yusuf telefonundan sosyal medyaya her girdiğinde şarjının 29 te 2 ‘si tükenmektedir. Yusuf sosyal medyaya iki günde kaç defa girdiğini hesaplayarak şarjını daha verimli kullanmak istiyor. Birinci gün 5, kinci gün ise 3 kere sosyal medyaya giren Yusuf şarjının toplam kaçta kaçını harcamıştır?`, // soru
+    ['0,541','0,451','0,551','0,541'], // şıklar
+    '0,551'
+]
+
+
+
+
+
+
+
+
+
+var soru13= [
+    1, // soru numarası
+
+    'Çevre uzunluğu 32,16 cm olan karenin bir kenar uzunluğu kaç cm dir?', // soru
+    ['80,04','64,32','96,48','128,64'], // şıklar
+    '80,04'
+]
+var soru14= [
+    2, // soru numarası
+
+    '72,047 ondalık gösteriminin çözümlenmiş hâli aşağıdakilerden hangisidir? ', // soru
+    ['a (7x10)+(2x1)+(4x0,1)+(7x0,0001)','b (7x10)+(2x1)+(4x0,1)+(7x0,001)','c (7x10)+(2x1)+(4x0,1)+(7x0,0001) ','d (7x10)+(2x1)+(4x0,01)+(7x0,001)'], // şıklar
+    'd'
+]
+var soru15= [
+    3, // soru numarası
+
+    '74,42A sayısının yüzde birler basamağına yuvarlanmış hâli 74,43’tür. Buna göre, A yerine aşağıdaki rakamlardan hangisi gelebilir?', // soru
+    ['2','3','4','5'], // şıklar
+    '5'
+]
+var soru16= [
+    4, // soru numarası
+
+    `Emirhan yeni aldığı güneş panellerinin kaç volt elektrik ürettiğini öğrenmek için evindeki en çok güneş alan yere güneş panellerini koyar ve ölçü aleti ile ölçmeye başlar. Emirhan güneş panellerinin İlk 7 saniye saniyede 2,24 volt ve sonraki 7 saniye saniyede de 3,34 ürettiğini gözlemliyor. Buna göre güneş panelleri 14 saniyede toplam kaç volt üretmiştir?`, // soru
+    ['39,06','37,14','39,60','37,41'], // şıklar
+    '39,06'
+]
+
+
+
+
+
+
+
+
+var soru17= [
+    1, // soru numarası
+
+    '24,54 kg çilek reçeli, 12 adet kavanoza eşit olarak paylaştırılacaktır. Buna göre, her bir kavanozda kaçar kg çilek reçeli olur?', // soru
+    ['2,445','2,45','2,315','2,045'], // şıklar
+    '2,045'
+]
+var soru18= [
+    2, // soru numarası
+
+    'Aşağıdakilerden hangisi yanlıştır?', // soru
+    ['603,04=6.100+3x1+4x0,01 ','704,07=7x100+4.1+7x0,1','60,20=6x10+2x0,1','48,725=4x10+8x1+7x0,1+2x0,01+5x0,001'], // şıklar
+    '704,07=7x100+4.1+7x0,1'
+]
+var soru19= [
+    3, // soru numarası
+
+    'Hangisi birler basamağına yuvarlandığında 42 onda birler basamağına yuvarlandığında 41,5 olur?', // soru
+    ['41,38','41,49','41,52','41,59'], // şıklar
+    '41,52'
+]
+var soru20= [
+    4, // soru numarası
+
+    `Hasan bir parfüm şişesinin her basmada kaç ml parfüm sıktığını ölçmek istiyor. İçinde 550 ml parfüm olan bir şişeyi 35,5 basmada bitirdiğine göre parfüm şişesi her basmada kaç ml parfüm sıkmaktadır?`, // soru
+    ['2,763','1,549','1,634','1,542'], // şıklar
+    '1,549'
+]
+
+
+
+
+
+
+var soru21= [
+    1, // soru numarası
+
+    'Aylık cep telefonu faturası 34,75 TL ile sabit olan Büşra, 1 yılda kaç TL cep telefonu faturası öder?', // soru
+    ['347,5','417','427','447,5'], // şıklar
+    '417'
+]
+var soru22= [
+    2, // soru numarası
+
+    `63,063 = (3x1) + (6x10) +…+ (3x0,001) 
+    Yukarıda karışık halde verilen çözümlemenin doğru olması için noktalı yere aşağıdakilerden hangisi gelmelidir? 
+    `, // soru
+    ['0,06','0,6','0,3','0,03'], // şıklar
+    '0,06'
+]
+var soru23= [
+    3, // soru numarası
+
+    '2,785 ondalık gösteriminin onda birler basamağına göre yuvarlanmış hâli aşağıdakilerden hangisidir?', // soru
+    ['2,8','2,79','2,7','2,78'], // şıklar
+    '2,8'
+]
+var soru24= [
+    4, // soru numarası
+
+    `Derviş bir mıknatısa tel sarıp mıknatısı çevirdiğinde telin elektrik ürettiğini fark etmektedir. Derviş mıknatısı 7 saniye çevirdiğinde her saniye 0,753 volt elektrik ürettiğini far ediyor. Daha sonra mıknatısın etrafına daha çok tel sararak tekrar 7 saniye çeviriyor ve saniyede 1,537 volt elektrik ürettiğini far ediyor. Buna göre Derviş 14 saniyede toplam kaç volt elektrik üretmiştir?`, // soru
+    ['17,36','36,17','16,03','30,16'], // şıklar
+    '16,03'
+]
+var main= document.querySelector('main');
+var nav= document.querySelector('nav');
+
+
+
+document.querySelector('#durum').addEventListener('click',function(){
+
+    document.querySelector('#Mavi').innerHTML=`
+      Toplam doğru yanıt: ${localStorage.getItem('Mavi')}<br><br><br><br>
+      Puan: ${localStorage.getItem('Mavi')*25}
+        <br>
+    `;
+
+
+    document.querySelector('#Yeşil').innerHTML=`
+      Toplam doğru yanıt: ${localStorage.getItem('Yeşil')}<br><br><br><br>
+      Puan: ${localStorage.getItem('Yeşil')*25}
+        <br>
+    `;
+
+
+    document.querySelector('#Kırmızı').innerHTML=`
+      Toplam doğru yanıt: ${localStorage.getItem('Kırmızı')}<br><br><br><br>
+      Puan: ${localStorage.getItem('Kırmızı')*25}
+        <br>
+    `;
+
+
+    document.querySelector('#Mor').innerHTML=`
+      Toplam doğru yanıt: ${localStorage.getItem('Mor')}<br><br><br><br>
+      Puan: ${localStorage.getItem('Mor')*25}
+        <br>
+    `;
+
+
+    document.querySelector('#Turuncu').innerHTML=`
+      Toplam doğru yanıt: ${localStorage.getItem('Turuncu')}<br><br><br><br>
+      Puan: ${localStorage.getItem('Turuncu')*25}
+        <br>
+    `;
+
+
+    document.querySelector('#Pembe').innerHTML=`
+      Toplam doğru yanıt: ${localStorage.getItem('Pembe')}<br><br><br><br>
+      Puan: ${localStorage.getItem('Pembe')*25}
+        <br>
+    `;
+
+
+   
+
+})
+
+
+
+
+document.querySelector('.clearls').addEventListener('click',()=>{
+    
+    var a=confirm('Tüm grupların verilerini silmek istediğinizden emin misiniz?')
+    if(a==true){
+    localStorage.clear();
+    console.log('Tüm veriler temizlendi.')}else{
+        console.log('işlem iptal edildi.')
+    }
+})
+
+var item = document.querySelectorAll('.main-item');
+item.forEach((item)=>{
+
+
+        if(item.id==='Mavi'){
+            var leyley=[soru1,soru2,soru3,soru4];
+        }
+        if(item.id==='Yeşil'){
+            var leyley=[soru5,soru6,soru7,soru8];
+        }
+        if(item.id==='Kırmızı'){
+            var leyley=[soru9,soru10,soru11,soru12];
+        }
+        if(item.id==='Mor'){
+            var leyley=[soru13,soru14,soru15,soru16];
+        }
+        if(item.id==='Turuncu'){
+            var leyley=[soru17,soru18,soru19,soru20];
+        }
+        if(item.id==='Pembe'){
+            var leyley=[soru21,soru22,soru23,soru24];
+        }
+     
+
+
+
+
+       
+
+
+
+
+
+
+
+        
+       if(localStorage.getItem(item.id)==null){
+        
+       }else{
+        var clas= document.getElementById(item.id);
+        clas.innerHTML=`
+        ${clas.id} grup yarışmaya katıldı. Sonuçları tüm durumu görüntüle butonundan öğrenebilirsiniz.`
+
+
+       }
+
+
+
+
+
+       if(localStorage.getItem(item.id)==null){
+      
+       }else{
+        var clas= document.getElementById(item.id);
+        clas.innerHTML=`
+        ${clas.id} grup yarışmaya katıldı. Sonuçları tüm durumu görüntüle butonundan öğrenebilirsiniz.`
+
+
+       }
+
+
+
+    
+
+
+
+
+    item.addEventListener('click',()=>{
+        
+
+
+        if(localStorage.getItem(item.id)===null){
+            console.log('Daha başlamamış.');
+       
+
+            main.style.display='flex';
+            main.style.justifyContent='center';
+            main.style.alignItems='center';
+            test(item.id);
+            
+        }else{
+            
+            main.innerHTML=`
+            <div class="event" style="text-align:center;">
+            <h4 class="soru">Bu grup daha önce etkinliğe katılmıştır. Tekrar katılabilmek için sağ taraftakı menüden gruba özel verileri sıfırlayabilirsiniz.</h4>
+            <br>
+        </div>
+    
+            `
+            nav.innerHTML=`
+    <h1>${item.id} Grup</h1>
+    <a href="ondaliketkinlik.html" class="tt">
+    <button class="onceki" id="anasayfa">Anasayfa</button>
+    </a>
+    <a href="ondaliketkinlik.html" class="tt">
+    <button class="onceki" id="lstclear">Bu grubun verilerini sıfırla</button>
+</a>
+
+    `;
+    document.querySelector('#lstclear').addEventListener('click',()=>{
+
+        localStorage.removeItem(item.id)
+
+    })
+
+
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+function test(id,sorunum){
+    var sorunum;
+    if(sorunum==undefined){
+        sorunum=0;
+    }
+
+
+
+
+
+
+nav.innerHTML=`
+<h1>${id} Grup</h1>
+<h1 id="zmnzmn"></h1>
+
+
+
+<a href="ondaliketkinlik.html" class="tt">
+<button class="onceki" id="anasayfa">Anasayfa</button>
+</a>
+`
+var nums=leyley[sorunum][0]
+var zoru=leyley[sorunum][1]
+var cevap=leyley[sorunum][3]
+var s1=leyley[sorunum][2][0]
+var s2=leyley[sorunum][2][1]
+var s3=leyley[sorunum][2][2]
+var s4=leyley[sorunum][2][3]
+
+main.innerHTML=`
+<div class="event">
+<h4 class="soru">${nums} ) ${zoru}</h4>
+<br>
+<button class="bb p" id="bir">A) <p class="a ib">${s1}</p></button> 
+<br>
+<button class="bb p" id="iki">B) <p class="b ib">${s2}</p></button> 
+<br>
+<button class="bb p" id="uc">C) <p class="c ib">${s3}</p></button> 
+<br>
+<button class="bb p" id="dort">D) <p class="d ib">${s4}</p></button> 
+<br>
+
+</div>
+
+`;
+var puan;
+if(puan==undefined){
+    puan=0;
+}
+document.querySelectorAll('.p').forEach((as)=>{
+as.addEventListener('click',function(){
+
+     if(sorunum==3){
+        var sizz=as.textContent.split(' ');
+        console.log('soru 3')
+        if(sizz[1]==cevap){
+            puan++;
+            console.log('Doğru cevap')
+            if(localStorage.getItem(id)==null){
+                localStorage.setItem(id,'1')
+            }else{
+                var pzr= JSON.stringify(localStorage.getItem(id))
+                var llls= Number(pzr[1])
+                localStorage.setItem(id,llls+1)
+                console.log(pzr[1])
+            }
+        }
+        main.innerHTML=`Tebrikler testi tamamladınız! 
+        <br>
+        Anasayfaya dönebilirsiniz.
+        `
+     }
+    
+    else{
+        var sizz=as.textContent.split(' ');
+        
+        if(sizz[1]==cevap){
+            puan++;
+            console.log('Doğru cevap')
+            if(localStorage.getItem(id)==null){
+                localStorage.setItem(id,'1')
+            }else if(sorunum==3){
+                console.log('selam')
+
+            }else{
+                var pzr= JSON.stringify(localStorage.getItem(id))
+                var llls= Number(pzr[1])
+                localStorage.setItem(id,llls+1)
+                console.log(pzr[1])
+            }
+        }
+        console.log(puan)
+        sorunum++;
+        console.log(sorunum+'dsfsdjkşf')
+        test(id,sorunum)
+        
+    }
+
+   
+
+
+
+})})
+
+}
+
+
+
+
+
+
+
+
+   
+
+
+
+})
